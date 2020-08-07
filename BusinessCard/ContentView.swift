0c4e3add4e7e0c759c83ee9e78950222ca6cb5ae
Jon Sweeney <jonsweeney26@gmil.com>
2020-08-07 10:08:52 -0700

    Initial Commit

---
+++ BusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BusinessCard
+//
+//  Created by Jonathan Sweeney on 8/7/20.
+//  Copyright © 2020 Jonathan Sweeney. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
